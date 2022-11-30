ccf0edb3c29d14fc6a565bb21730fbf44d560b99
Jari Roossien <jariroossien@gmail.com>
2022-11-30 22:18:32 +0100

    Project Set up

---
+++ src/nl/dizmizzer/aoc/days/Day01.java
@@ -0,0 +1,13 @@
+package nl.dizmizzer.aoc.days;
+
+public class Day01 extends Day {
+    @Override
+    public long solveOne() {
+        return 0;
+    }
+
+    @Override
+    public long solveTwo() {
+        return 0;
+    }
+}
