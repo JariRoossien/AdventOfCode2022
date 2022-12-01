10ec28ee7d640ef820d4a7be0a4ad47f55876302
Jari Roossien <jariroossien@gmail.com>
2022-12-01 06:16:38 +0100

    Day 1 finished

--- src/nl/dizmizzer/aoc/days/Day01.java
+++ src/nl/dizmizzer/aoc/days/Day01.java
@@ -1,13 +1,39 @@
 package nl.dizmizzer.aoc.days;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Day01 extends Day {
     @Override
     public long solveOne() {
-        return 0;
+        PriorityQueue<Integer> integerQueue = new PriorityQueue<>(Comparator.comparing(Integer::intValue).reversed());
+        int sum = 0;
+        for (String s : input) {
+            if (s.trim().equals("")) {
+                integerQueue.add(sum);
+                sum = 0;
+            } else {
+                sum += Integer.parseInt(s);
+            }
+        }
+        return integerQueue.poll();
     }
 
     @Override
     public long solveTwo() {
-        return 0;
+        PriorityQueue<Integer> integerQueue = new PriorityQueue<>(Comparator.comparing(Integer::intValue).reversed());
+        int sum = 0;
+        for (String s : input) {
+            if (s.trim().equals("")) {
+                integerQueue.add(sum);
+                sum = 0;
+            } else {
+                sum += Integer.parseInt(s);
+            }
+        }
+        int first = integerQueue.poll();
+        int second = integerQueue.poll();
+        int third = integerQueue.poll();
+        return first + second + third;
     }
 }
